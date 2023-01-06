31f462e4eba88a9d6268d741567f0a4e533522dd
Maurício Casagrande <mauricio.araras@hotmail.com>
2023-01-06 16:01:42 -0300

    0603 1600

--- frontend/src/components/CardsComponent.vue
+++ frontend/src/components/CardsComponent.vue
@@ -1,34 +1,57 @@
 <template>
-    <div class="my_cards">
+    <div :class="{'my_cards_up':true, my_cards_down:isDown}">
         <div class="content top">
-            <h3>Alunos</h3>
+            <h3>{{ type }}</h3>
             <div class="percentage">
-                <i class="fa-solid fa-arrow-up"></i>
-                &nbsp;&nbsp;<p>90%</p>
+                <i :class=[updown]></i>
+                &nbsp;&nbsp;<p>{{ percentage }}</p>
             </div>
         </div>
 
         <div class="content footer">
-            <i class="fa-solid fa-graduation-cap"></i>
-            <p class="qtd">17900</p>
+            <i :class=[icon]></i>
+            <p class="qtd">{{ qtd }}</p>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    name: 'CardsComponent',
+    data() {
+        return {
+           isDown: false
+        }
+    },
+    props: {
+        type: String,
+        updown: String,
+        percentage: String,
+        icon: String,
+        qtd: String
+    }
 }
 </script>
 
 <style scoped>
 
-.my_cards {
-    width: 205px;
+.my_cards_up {
+    width: 100%;
     display: flex;
     flex-direction: column;
-    padding: 20px;
+    padding: 15px;
     border: 3px solid rgb(2, 196, 2);
+    border-radius: 15px;
+    background-color: white;
+    color: #37485c;
+}
+
+.my_cards_down {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 15px;
+    border: 3px solid rgb(255, 20, 20);
     border-radius: 15px;
     background-color: white;
     color: #37485c;
@@ -43,13 +66,19 @@ export default {
     display: flex;
 }
 
-.percentage {
+.percentage{
     display: flex;
     color: rgb(2, 196, 2);
 }
 
 .footer {
     display: flex;
+    font-size: 25px;
+    margin: 0px;
+}
+
+.qtd {
+    margin: 0px;
 }
 
 </style>
--- frontend/src/components/Home/HomeComponent.vue
+++ frontend/src/components/Home/HomeComponent.vue
@@ -7,7 +7,49 @@
                 </header>
             </div>
 
-            <CardsComponent/>
+            <div class="row">
+                <div class="col-12 col-md-3">
+                    <CardsComponent 
+                        :type="'Alunos'"
+                        :updown="'fa-solid fa-arrow-up'" 
+                        :percentage="'90%'" 
+                        :icon="'fa-solid fa-graduation-cap'"
+                        :qtd="'17900'"
+                    />
+                </div>
+
+                <div class="col-12 col-md-3">
+                    <CardsComponent 
+                        :type="'Receita'" 
+                        :updown="'fa-solid fa-arrow-down'" 
+                        :percentage="'15%'" 
+                        :icon="'fa-solid fa-sack-dollar'"
+                        :qtd="'R$886,2 Mi'"
+                    />
+                </div>
+
+                <div class="col-12 col-md-3">
+                    <CardsComponent 
+                        :type="'Inscrições'" 
+                        :updown="'fa-solid fa-arrow-up'" 
+                        :percentage="'35%'" 
+                        :icon="'fa-solid fa-people-group'"
+                        :qtd="'7.000'"
+                    />
+                </div>
+
+                <div class="col-12 col-md-3">
+                    <CardsComponent 
+                        :type="'Abandonos'" 
+                        :updown="'fa-solid fa-arrow-up'" 
+                        :percentage="'18%'" 
+                        :icon="'fa-solid fa-heart-crack'"
+                        :qtd="'1.899'"
+                    />
+                </div>
+            </div>
+
+            
 
         </template>
     </DashboardComponent>
